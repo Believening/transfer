5524f69e8e768c7015cb649e8268ed767d68bba3
Believening <kang_417@yeah.net>
2018-11-01 15:08:00 +0800

    heapsort

---
+++ heap.go
@@ -0,0 +1,43 @@
+package main
+
+import "fmt"
+
+/*
+建一个最大堆，然后将堆顶和最后一个元素换位置
+重新建堆
+原地排序
+ */
+func heapSort(ints []int) {
+	s := len(ints) / 2 //最后一个有儿子
+	for i := s; i > -1; i-- { //建堆
+		heap(ints, i, len(ints)-1)
+	}
+	for i := len(ints) - 1; i > 0; i-- {
+		ints[i], ints[0] = ints[0], ints[i]
+		heap(ints, 0, i-1)
+	}
+}
+
+func heap(ints []int, start, end int) { //up
+	l := start*2 + 1
+	if l > end {
+		return
+	}
+	r := start*2 + 2
+	p := l
+	if r <= end && ints[l] > ints[r] {
+		p = r
+	}
+	if ints[p] > ints[start] {
+		return
+	}
+	ints[p], ints[start] = ints[start], ints[p]
+	heap(ints, p, end)
+}
+
+func main() {
+	nums := []int{3, 4, 78, 23, 4, 65, 23, 5, 45, 8, 2}
+	heapSort(nums)
+	fmt.Println(nums)
+
+}
